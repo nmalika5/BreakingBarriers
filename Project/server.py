1aa6c7cc665ac3270d2fdad81837637c60993b16
Malika Nikhmonova <malikanikhmonova@Malikas-MacBook-Air.local>
2016-02-25 11:08:54 -0800

    Added routes for CRUD. Fixed a bug in reply to text.

--- Project/server.py
+++ Project/server.py
@@ -15,7 +15,7 @@ import gmail_contacts
 import pickle
 import gdata.contacts.client
 import gmail_contacts
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
@@ -65,7 +65,6 @@ class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
                                         user2.language.yandex_lang_code)['text']
 
         room = self.room_name(user1.user_id, user2.user_id)
-        print "AAAAAAAAAAAAAAA" + " " + room
         self.emit_to_room(room, 'msg_to_room', 
                         self.socket.session['nickname'], 
                         str(trans_msg))
@@ -74,6 +73,7 @@ class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
     def recv_message(self, message):
         """Receive a message"""
         print "PING!!!", message
+
 
 app = Flask(__name__)
 
@@ -86,24 +86,8 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-
-    key = request.args.get("code")
-    
-    if key:
-        user_id = flask.session['user_id']
-        user = User.query.get(user_id)
-        token = pickle.load(open('token' + str(user_id) + '.p', 'rb'))
-        token.get_access_token(key)
-        client = gdata.contacts.client.ContactsClient(source='appname')
-        client = token.authorize(client)
-        feed = client.GetContacts()
-        contacts = gmail_contacts.parse_contacts(user, str(feed))
-
-        return redirect("/users/%s" % user.user_id)
-
-    else:
         
-        return render_template("home.html")
+    return render_template("home.html")
 
 
 @app.route('/register', methods=['GET'])
@@ -132,9 +116,11 @@ def register_process():
     lang_id = request.form.get("lang_id")
     phone_number = request.form.get("phone")
 
-    new_user = User(email=email, password=password, first_name=first_name,
+
+    new_user = User(email=email, first_name=first_name,
                     last_name=last_name, lang_id=lang_id, phone_number=phone_number)
 
+    new_user.set_password(password)
     db.session.add(new_user)
     db.session.commit()
 
@@ -156,13 +142,13 @@ def login_process():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    user = User.query.filter_by(email=email, password=password).first()
+    user = User.query.filter_by(email=email).first()
 
     if not user:
         flash("No such user")
         return redirect("/register")
 
-    if user.password != password:
+    if not user.check_password(password):
         flash("Incorrect password")
         return redirect("/login")
 
@@ -204,11 +190,14 @@ def user_detail(user_id):
 
     for contact in user_contact_list:
         contacts.append((contact.contact_phone, contact.language.lang_name, 
-                         contact.contact_first_name))
+                         contact.contact_first_name, contact.contact_last_name, 
+                         contact.contact_id))
 
-    return render_template("user_profile.html", user=user, contacts=contacts, 
-                            user_id=user_id)
+    return render_template("contact_edit.html", user=user, 
+                            user_id=user_id, contacts=json.dumps(contacts))
 
+    # return render_template("user_profile.html", user=user, 
+    #                         user_id=user_id, contacts=contacts)
 
 
     # TODO: create User controller that will handle user-related stuff
@@ -226,26 +215,48 @@ def show_user_edit(user_id):
     phone_number = user.phone_number
     lang_name = user.language.lang_name
 
+    language_list = Language.query.all()
+
+    languages = []
+
+    for lang in language_list:
+        languages.append((lang.lang_id, lang.lang_name))
+
  
     return render_template("user_edit.html", user_id=user_id, first_name=first_name,
                             last_name=last_name, email= email, password=password, 
-                            phone_number=phone_number, lang_name=lang_name)
+                            phone_number=phone_number, languages=languages)
 
 
 @app.route("/users/<int:user_id>/edit_user", methods=["POST"])
 def user_edit(user_id):
     """Edit user's info."""
 
-    user = User.query.get(user_id)
+    user = User.query.filter_by(user_id=user_id).one()
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    lang_id = request.form.get("lang_id")
+    phone_number = request.form.get("phone_number")
+
 
     if user:
-            user[0].first_name = first_name
-            user[1].last_name = last_name
-            user[2].email = email
-            user[3].password = password
-            user[4].lang_id = lang_id
-            user[5].phone_number= phone_number
-
+        if first_name: 
+            user.first_name = first_name
+        if last_name:
+            user.last_name = last_name
+        if email:
+            user.email = email
+        if password: 
+            user.password = password
+        if lang_id:
+            user.lang_id = lang_id
+        if phone_number:
+            user.phone_number = phone_number
+    
+    user.set_password(password)
     db.session.commit()
 
     return redirect("/users/%s" % user_id)
@@ -284,6 +295,46 @@ def add_contact(user_id):
 
     return redirect("/users/%s" % user_id)
 
+
+@app.route("/users/<int:user_id>/contacts/<int:contact_id>", methods=["DELETE"])
+def delete_contact(user_id, contact_id):
+    """Delete a contact."""
+
+    contact_id = Contact.query.filter_by(contact_id=contact_id).one()
+    
+    flash("Contact deleted.")
+    db.session.delete(contact_id)
+
+    db.session.commit()
+
+    return ('', 204) 
+
+@app.route("/users/<int:user_id>/contacts/<int:contact_id>", methods=["PUT"])
+def edit_contact(user_id, contact_id):
+    """Edit a contact."""
+
+    contact = Contact.query.filter_by(contact_id=contact_id).one()
+    request.data = json.loads(request.data)
+    contact_fname = request.data["fname"]
+    contact_lname = request.data["lname"]
+    language = request.data["language"]
+    phone = request.data["phone"]
+
+    if contact:
+        if contact_fname: 
+            contact.contact_first_name = contact_fname
+        if contact_lname:
+            contact.contact_last_name = contact_lname
+        if language:
+            lang = Language.query.filter_by(lang_name=language).one()
+            contact.lang_id = lang.lang_id
+        if phone:
+            contact.contact_phone = phone
+    
+    flash("Contact updated.")
+    db.session.commit()
+
+    return ('', 204) 
 
 @app.route("/users/<int:user_id>/send_text", methods=["GET"])
 def show_text_form(user_id):
@@ -328,23 +379,24 @@ def submit_text_form(user_id):
     contacts = user.contacts
 
     lang_code = user.language.yandex_lang_code
-    trans_msgs = []
     contact_langs = []
 
     #making a unique list of contacts' langs
+    unique_lang_dict = {}
     for contact in contacts:
         contact_lang = contact.language.yandex_lang_code
-        contact_langs.append(contact_lang)
-    unique_list_lang = list(set(contact_langs))
+        unique_lang_dict[contact.language.lang_id] = contact_lang
 
     trans_msgs_dict = {}
-    for unique_lang in unique_list_lang:
-        contact_lang_id = contact.language.lang_id
+    trans_msgs = []
+    for unique_lang_id in unique_lang_dict:
+        unique_lang_code = unique_lang_dict[unique_lang_id]
         contact_msg_id = message.message_id
-        trans_msg = yandex.translate_message(user_message, lang_code, contact_lang)['text']
-        trans_status = yandex.translate_message(user_message, lang_code, contact_lang)['code']
-        trans_msgs_dict[unique_lang] = trans_msg
-        msg_lang = MessageLang(lang_id=contact_lang_id, message_id=contact_msg_id, translated_message=trans_msg,
+        trans_msg = yandex.translate_message(user_message, lang_code, unique_lang_code)
+        trans_text = trans_msg['text']
+        trans_status = trans_msg['code']
+        trans_msgs_dict[unique_lang_code] = trans_text
+        msg_lang = MessageLang(lang_id=unique_lang_id, message_id=contact_msg_id, translated_message=trans_text,
                                message_status=trans_status)
         db.session.add(msg_lang)
     
@@ -363,14 +415,24 @@ def submit_text_form(user_id):
     flash('The translated texts have been sent')
     return redirect("/users/%s" % user_id)
 
-# @app.route("/users/<int:user_id>/messages", methods=["GET"])
-# def message_list(user_id):
-#     """Show info about user's messages."""
+@app.route("/users/<int:user_id>/messages", methods=["GET"])
+def message_list(user_id):
+    """Show info about user's messages."""
 
-#     messages = Message.query.all()
-#     user_message = Message.query.filter_by(user_id=user_id).all()
+    user_messages = Message.query.filter_by(user_id = user_id).all()
 
-#     return render_template("message_list.html", messages=messages)
+    messages = []
+
+    if len(user_messages) == 0:
+        flash("The user has no messages")
+        
+
+    for message in user_messages:
+        messages.append((message.message_id, message.message_text, 
+                         message.message_sent_at))
+
+
+    return render_template("message_list.html", messages=messages)
 
 
 @app.route("/users/<int:user_id>/send_text/preview", methods=["POST"])
@@ -379,7 +441,6 @@ def preview_message(user_id):
 
     user_message = request.form.get("text")
 
-    print user_message
     if user_message:
         user_id = session['user_id']
         user = User.query.filter_by(user_id=user_id).all()[0]
@@ -427,7 +488,7 @@ def send_text():
         # all contacts that belong to that user and get a first one that matches the phone number
         from_contact = Contact.query.filter((Contact.user_id==user.user_id) & (Contact.contact_phone==contact_phone)).first()
         from_lang = from_contact.language.yandex_lang_code
-        translated_resp_msg = yandex.translate_message(response_message, from_lang, to_lang)
+        translated_resp_msg = yandex.translate_message(response_message, from_lang, to_lang)['text']
         twilio_api.send_message(translated_resp_msg, user_phone)
 
         return ('', 204)
@@ -437,6 +498,7 @@ def send_text():
 
 #TODO send text & preview buttons which will return AJAX response with diff routes
 
+
 @app.route("/users/<int:user_id>/contacts", methods=["POST", "GET"])
 def redirect_gmail(user_id):
     """Redirect the user to gmail access page"""
@@ -444,6 +506,24 @@ def redirect_gmail(user_id):
     authorized_url = gmail_contacts.authorize_url(flask.session, user_id)
 
     return json.dumps(authorized_url)
+
+@app.route("/user_contacts", methods=["POST", "GET"])
+def redirect_app():
+    """Redirect to my app after gmail"""
+
+    key = request.args.get("code")
+    
+    if key:
+        user_id = flask.session['user_id']
+        user = User.query.get(user_id)
+        token = pickle.load(open('token' + str(user_id) + '.p', 'rb'))
+        token.get_access_token(key)
+        client = gdata.contacts.client.ContactsClient(source='appname')
+        client = token.authorize(client)
+        feed = client.GetContacts()
+        contacts = gmail_contacts.parse_contacts(user, str(feed))
+
+        return redirect("/users/%s" % user.user_id)
 
 @app.route('/users/<int:user1>/chat/<int:user2>')
 def chat(user1, user2):
